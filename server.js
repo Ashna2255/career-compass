a326ea500e869e6b3334a47dd6cd2aca17ef394b
Ashna2255 <Ashnaroseantony@gmail.com>
2026-07-04 15:31:40 +0530

    Remove deprecated Mongoose connection options

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/booking', bookingRoutes);
 app.use('/feedback', feedbackRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error(err));
 
